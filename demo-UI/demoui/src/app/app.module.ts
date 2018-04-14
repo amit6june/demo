06b0f2ca08786b0216bc2ff915b697e8eb9bc464
Amit Mishra <kumar.amit.mishra@gmail.com>
2018-04-14 20:47:03 +1000

    2nduser

--- demo-UI/demoui/src/app/app.module.ts
+++ demo-UI/demoui/src/app/app.module.ts
@@ -15,7 +15,7 @@ import {HttpClientModule} from "@angular/common/http";
   declarations: [
     AppComponent,
     UserComponent,
-    AddUserComponent 
+    AddUserComponent   
   ],
   imports: [
     BrowserModule,
